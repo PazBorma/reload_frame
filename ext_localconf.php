9c878869970d818aa2e9071829cbc4c95a53a253
Thomas Ebermann <Thomas.Ebermann@sunzinet.com>
2020-11-08 17:03:01 +0100

    remove strict type declaration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,3 @@
 <?php
-declare(strict_types=1);
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook'][] = 'PazBorma\ReloadFrame\Hooks\ReloadFrameHook->addReloadFrameButton';
